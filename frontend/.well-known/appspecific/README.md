a94f3014e192e3480d71c57ef8645739878b460c
Lawrence Agnir <agnir.lawrence@gmail.com>
2026-04-23 07:05:33 +0800

    Adding Events for login

---
+++ frontend/.well-known/appspecific/README.md
@@ -0,0 +1,2 @@
+This file right here is to serves as to catch error from the Framework Mode of React Router Dev.
+Empty Json file should work with the folder path.
--- frontend/app/pages/login.tsx
+++ frontend/app/pages/login.tsx
@@ -1,4 +1,5 @@
 import type { Route } from "./+types/login";
+import { useState } from "react";
 
 export function meta({}: Route.MetaArgs){
     return [
@@ -13,17 +14,36 @@ export function meta({}: Route.MetaArgs){
 }
 
 function login() {
+
+    const [username, setUsername] = useState<string | undefined>();
+    const [password, setPassword] = useState<string | undefined>();
+
+    async function handlesubmit(e: React.ChangeEvent<HTMLFormElement>){
+        e.preventDefault();
+        console.log("trigger")
+        console.log(username)
+        console.log(password)
+    }
+
+    async function onChangeUsername(e: React.ChangeEvent<HTMLInputElement>){
+        setUsername(e.target.value)
+    }
+
+    async function onChangePassword(e: React.ChangeEvent<HTMLInputElement>){
+        setPassword(e.target.value)
+    }
+
     return (
         <>
             <div className="bg-white text-black content-center width-20">
                 <h3>Login Account</h3>
-                <form>
+                <form onSubmit={handlesubmit}>
                     <fieldset>
-                        <label htmlFor="username">Username/Email</label>
-                        <input type="text" name="login_username" />
+                        <label htmlFor="login_username">Username/Email</label>
+                        <input id="login_username" type="text" name="login_username" onChange={onChangeUsername} />
                         <br />
-                        <label htmlFor="password">Password</label>
-                        <input type="password" name="login_password" />
+                        <label htmlFor="login_password">Password</label>
+                        <input id="login_password" type="password" name="login_password" onChange={onChangePassword} />
                         <br />
                         <input type="submit" value="Login" />
                     </fieldset>
