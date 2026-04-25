72450d4b71adf11592841bfa704c1abd88b980fe
Lawrence Agnir <agnir.lawrence@gmail.com>
2026-04-25 13:10:48 +0800

    Added mermaid diagram and added app in backend

--- backend/webconfig/settings.py
+++ backend/webconfig/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'webconfig.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': "postgres",
+        'PASSWORD': "root",
+        'HOST': "127.0.0.1",
+        'PORT': "5432"
     }
 }
 
